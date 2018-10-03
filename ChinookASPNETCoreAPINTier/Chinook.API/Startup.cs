07fbafff9158c14d7d95b863e7abfbf7a3b53508
Chris Woodruff <cwoodruff@live.com>
2018-10-03 10:23:40 -0400

    added response and in-memory caching examples

--- ChinookASPNETCoreAPINTier/Chinook.API/Startup.cs
+++ ChinookASPNETCoreAPINTier/Chinook.API/Startup.cs
@@ -20,6 +20,10 @@ namespace Chinook.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMemoryCache();
+            
+            services.AddResponseCaching();
+            
             services.AddMvc();
 
             services.ConfigureRepositories()
